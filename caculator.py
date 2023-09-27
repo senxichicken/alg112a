fd8929da6e2c8a1443120536ea17d7754f4db145
senxichicken <99935001+senxichicken@users.noreply.github.com>
2023-09-27 10:58:38 +0800

    0927
    迭代法

---
+++ caculator.py
@@ -0,0 +1,11 @@
+import math 
+
+f1 = lambda x : (3+ 5**0.5)/2 
+f2 = lambda x : (x*x +1)/3
+f3 = lambda x : 3-(1/x)
+
+x1 = x2 = x3 = 1
+
+for i in range(20):
+    x1, x2, x3 = f1(x1), f2(x2), f3(x3)
+    print('x1:', x1, 'x2', x2, 'x3', x3)
